a55bf60bbc0cea8cf9a939f6b456926e256aa462
Arina Safiullina <31733496+arinasaf11@users.noreply.github.com>
2018-06-25 19:32:35 +0300

    Add files via upload

--- scr/index.js
+++ scr/index.js
@@ -1,16 +1,16 @@
-{
-  "scripts" : {
-    "start" : "nodemon"
-	},
-  "name": "express_project",
-  "version" : "1.0.0",
-  "main" : "./src/index.js",
-  "dependencies": {
-    "axios": "^0.18.0",
-    "express": "^4.16.3",
-    "pug": "^2.0.3"
-  },
-  "devDependencies": {
-    "nodemon": "^1.17.5"
-  }
-}
+const express = require('express');
+const { get } = require('axios');
+const URL = 'https://kodaktor.ru/j/users';
+
+const PORT = 4321;
+const app = express();
+app
+   .get(/hello/, r => r.res.end('Hello, world!'))
+   .get(/users/, async r => { 
+      const { data: { users: items } } = await get(URL);
+      r.res.render('list', {title: 'Login list', items});
+   })
+   .use(r => r.res.status(404).end('Still not here, sorry!'))
+   .use((e, r, res, n) => res.status(500).end('Error: ${e}'))
+   .set('view engine', 'pug')
+.listen(process.env.PORT || PORT, () => console.log(process.pid));
